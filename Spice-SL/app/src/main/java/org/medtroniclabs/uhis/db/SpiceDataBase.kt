0246f7735c2f04370fae08cb19be97dfb713f77c
Sahjad Ansari <sahjad.ansari@medtroniclabs.org>
2026-06-09 16:56:23 +0000

    Merged in import_fix_sz (pull request #2685)
    import issue fix
    
    * import issue fix
    
    
    Approved-by: M Venkatesh Prasad

--- Spice-SL/app/src/main/java/org/medtroniclabs/uhis/db/SpiceDataBase.kt
+++ Spice-SL/app/src/main/java/org/medtroniclabs/uhis/db/SpiceDataBase.kt
@@ -1,6 +1,7 @@
 package org.medtroniclabs.uhis.db
 
 import android.content.Context
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
