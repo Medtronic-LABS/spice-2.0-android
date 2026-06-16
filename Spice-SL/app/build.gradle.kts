7d6a78a090cb5a4c8845a4d95390550f37dcbbf9
Epari Nigam Prasad Patro <nigam@innoventes.co>
2026-06-16 09:00:03 +0000

    Merged in verify_force_update_release (pull request #2703)
    Verify force update release
    
    *  - Release version 24 with force update changes.
    
    *  - Release version 25 to test force upgrade.

--- Spice-SL/app/build.gradle.kts
+++ Spice-SL/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "org.medtroniclabs.uhis"
         minSdk = 23
         targetSdk = 36
-        versionCode = 23
-        versionName = "1.0.3"
+        versionCode = 25
+        versionName = "1.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
         missingDimensionStrategy("version", "production")
